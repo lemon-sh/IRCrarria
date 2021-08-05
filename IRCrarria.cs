dd1c5a2f5e03176f53fac87367db0f2d18342214
lemon-sh <pawel.stolarski7@gmail.com>
2021-08-05 15:22:12 +0200

    Add test implementation code

--- IRCrarria.cs
+++ IRCrarria.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using IrcDotNet;
 using Terraria;
 using TerrariaApi.Server;
 using TShockAPI;
@@ -9,6 +10,10 @@ namespace IRCrarria
     [ApiVersion(2, 1)]
     public class IRCrarria : TerrariaPlugin
     {
+        private const string IRCurl = "";
+        private StandardIrcClient irc;
+        private IrcChannel ircChannel;
+        
         public override string Author => "lemon-sh";
         public override string Name => "IRCrarria";
         public override string Description => "IRC<->Terraria bridge that actually works with new TShock versions";
@@ -19,11 +24,23 @@ namespace IRCrarria
         public override void Initialize()
         {
             PlayerHooks.PlayerChat += OnChat;
+            irc = new StandardIrcClient();
+            irc.Registered += (sender, args) =>
+            {
+                irc.LocalUser.JoinedChannel += (sender2, args2) =>
+                {
+                    ircChannel = args2.Channel;
+                };
+                irc.Channels.Join("#main");
+            };
+            irc.Connect(new Uri(IRCurl), new IrcUserRegistrationInfo {
+                NickName = "ircrarria", UserName = "ircrarria", RealName = "IRCrarria"
+            });
         }
 
-        private static void OnChat(PlayerChatEventArgs ev)
+        private void OnChat(PlayerChatEventArgs ev)
         {
-            TShock.Utils.Broadcast($"[c/FF66FF:{ev.RawText}]", Microsoft.Xna.Framework.Color.LightCyan);
+            irc.LocalUser.SendMessage(ircChannel, ev.RawText);
         }
 
         protected override void Dispose(bool disposing)
