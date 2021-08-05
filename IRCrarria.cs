9a20d407357d3e25c93d787b6632efb62f4b7b14
lemon-sh <pawel.stolarski7@gmail.com>
2021-08-05 14:19:10 +0200

    Not-so-initial commit (import project)

---
+++ IRCrarria.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+using TShockAPI.Hooks;
+
+namespace IRCrarria
+{
+    [ApiVersion(2, 1)]
+    public class IRCrarria : TerrariaPlugin
+    {
+        public override string Author => "lemon-sh";
+        public override string Name => "IRCrarria";
+        public override string Description => "IRC<->Terraria bridge that actually works with new TShock versions";
+        public override Version Version => new Version(1, 0, 0, 0);
+
+        public IRCrarria(Main game) : base(game) { }
+
+        public override void Initialize()
+        {
+            PlayerHooks.PlayerChat += OnChat;
+        }
+
+        private static void OnChat(PlayerChatEventArgs ev)
+        {
+            TShock.Utils.Broadcast($"[c/FF66FF:{ev.RawText}]", Microsoft.Xna.Framework.Color.LightCyan);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                PlayerHooks.PlayerChat -= OnChat;
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
