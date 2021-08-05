fc155a6443b38aa99003fa580c57046cc7ad9436
lemon-sh <pawel.stolarski7@gmail.com>
2021-08-05 16:44:03 +0200

    Add a config manager

---
+++ Config.cs
@@ -0,0 +1,30 @@
+﻿using Tomlyn;
+using Tomlyn.Model;
+
+namespace IRCrarria
+{
+    public class Config
+    {
+        public string Hostname { get; }
+        public int Port { get; }
+        public bool UseSsl { get; }
+        
+        public string Username { get; }
+        public string Nickname { get; }
+        public string Channel { get; }
+        
+
+        public Config(string configText)
+        {
+            var document = Toml.Parse(configText).ToModel();
+            var hosttable = (TomlTable) document["host"];
+            Hostname = (string) hosttable["hostname"];
+            Port = (int)(long) hosttable["port"]; 
+            UseSsl = (bool) hosttable["ssl"];
+            var irctable = (TomlTable) document["irc"];
+            Username = (string) irctable["username"];
+            Nickname = (string) irctable["nickname"];
+            Channel = (string) irctable["channel"];
+        }
+    }
+}
