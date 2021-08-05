67ca1b6fdae35a8d9c0804ae5e4effbc7335dc4e
lemon-sh <pawel.stolarski7@gmail.com>
2021-08-05 17:44:59 +0200

    Add server join/leave/broadcast notifications

--- IRCrarria.cs
+++ IRCrarria.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 using IrcDotNet;
 using Microsoft.Xna.Framework;
 using Terraria;
@@ -19,7 +20,7 @@ namespace IRCrarria
 
         private static readonly string ConfigPath = Path.Combine(TShock.SavePath, "ircrarria.toml");
 
-        private Config _cfg;
+        private readonly Config _cfg;
         private StandardIrcClient _irc;
         private IrcChannel _ircChannel;
 
@@ -31,12 +32,19 @@ namespace IRCrarria
 
         public override void Initialize()
         {
+            // register hooks
+            ServerApi.Hooks.ServerJoin.Register(this, OnJoin);
+            ServerApi.Hooks.ServerLeave.Register(this, OnLeave);
+            ServerApi.Hooks.ServerBroadcast.Register(this, OnBroadcast);
             PlayerHooks.PlayerChat += OnChat;
+
+            // connect to the IRC server
             _irc = new StandardIrcClient();
             _irc.Registered += (isn, iarg) =>
             {
                 _irc.LocalUser.JoinedChannel += (jsn, jarg) =>
                 {
+                    if (_ircChannel != null) return;
                     jarg.Channel.MessageReceived += (ksn, karg) =>
                     {
                         TShock.Utils.Broadcast($"[c/CE1F6A:IRC] [c/FF9A8C:{karg.Source.Name}] {karg.Text.StripNonAscii()}", Color.White);
@@ -50,20 +58,50 @@ namespace IRCrarria
             });
         }
 
-        private void OnChat(PlayerChatEventArgs ev)
-        {
-            _irc.LocalUser.SendMessage(_ircChannel, $"<{ev.Player.Name}> {ev.RawText}");
-            TShock.Utils.Broadcast($"[c/28FFBF:Terraria] [c/BCFFB9:{ev.Player.Name}] {ev.RawText.StripNonAscii()}", Color.White);
-            ev.Handled = true;
-        }
-
         protected override void Dispose(bool disposing)
         {
             if (disposing)
             {
+                ServerApi.Hooks.ServerJoin.Deregister(this, OnJoin);
+                ServerApi.Hooks.ServerBroadcast.Deregister(this, OnBroadcast);
+                ServerApi.Hooks.ServerLeave.Deregister(this, OnLeave);
                 PlayerHooks.PlayerChat -= OnChat;
+                _irc.Disconnect();
+                _irc.Dispose();
             }
             base.Dispose(disposing);
+        }
+        
+        private void OnChat(PlayerChatEventArgs ev)
+        {
+            _irc.LocalUser.SendMessage(_ircChannel, $"\x00039<\x00038{ev.Player.Name}\x00039>\x3 {ev.RawText}");
+            TShock.Utils.Broadcast($"[c/28FFBF:Terraria] [c/BCFFB9:{ev.Player.Name}] {ev.RawText.StripNonAscii()}", Color.White);
+            ev.Handled = true;
+        }
+
+        private void OnJoin(JoinEventArgs args)
+        {
+            _irc.LocalUser.SendMessage(_ircChannel, $"\x00038{args.Who}\x00039 joined the game.");
+        }
+
+        private void OnLeave(LeaveEventArgs args)
+        {
+            _irc.LocalUser.SendMessage(_ircChannel, $"\x00038{args.Who}\x00034 left the game.");
+        }
+        
+        private void OnBroadcast(ServerBroadcastEventArgs args)
+        {
+            _irc.LocalUser.SendMessage(_ircChannel, $"\x000311{args.Message}");
+        }
+    }
+    
+    public static class StringExtensions
+    {
+        public static string StripNonAscii(this string str)
+        {
+            return Encoding.ASCII.GetString(Encoding.Convert(Encoding.UTF8,
+                Encoding.GetEncoding(Encoding.ASCII.EncodingName, new EncoderReplacementFallback(string.Empty),
+                    new DecoderExceptionFallback()), Encoding.UTF8.GetBytes(str)));
         }
     }
 }
